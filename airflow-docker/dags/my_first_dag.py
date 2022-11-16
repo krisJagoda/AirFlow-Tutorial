e016a9bb065dd807e3df5792ba0a7ff9ff98e375
Krystian Jagoda <krystian.jagoda@outlook.com>
2022-11-17 00:38:42 +0100

    Add first running DAG with three tasks

---
+++ airflow-docker/dags/my_first_dag.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.operators.bash import BashOperator
+from sqlalchemy_utils.types.enriched_datetime.pendulum_datetime import pendulum
+
+default_args = {
+    'owner': 'kjagoda',
+    'retries': 3,
+    'retry_delay': timedelta(seconds=120)
+}
+local_tz = pendulum.timezone('Europe/Warsaw')
+
+with DAG(
+    dag_id='my_first_dag',
+    description='This is my first dag that I have written',
+    default_args=default_args,
+    start_date=datetime(2022, 11, 16, tzinfo=local_tz),
+    schedule_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='first_task',
+        bash_command="echo Hello World!"
+    )
+
+    task2 = BashOperator(
+        task_id='second_task',
+        bash_command="echo This is the second task which needs to be executed."
+    )
+
+    task3 = BashOperator(
+        task_id='third_task',
+        bash_command="echo Third task will execute after the second one completes successfully."
+    )
+
+    task1.set_downstream(task2)
+    task2.set_downstream(task3)
